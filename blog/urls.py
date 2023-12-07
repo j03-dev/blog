4df2c61f6eebc579826043d5c06748dc18ef8204
j03-dev <24nomeniavo@gmail.com>
2023-12-07 14:02:34 +0300

    add to static on url

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,6 @@ from django.urls import path, include
 urlpatterns = [
       path('admin/', admin.site.urls),
       path('', include("core.urls"))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
