b3444002301df44d82807b84ff707f171192e9e9
FITAHIANA Nomeniavo joe <24nomeniavo@gmail.com>
2025-05-02 18:02:45 +0300

    remove repetion in the code

--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
 from oxapy import get, delete, put, post, Request, templating, Status, Redirect
-
+from functools import wraps
 from sqlalchemy.orm import Session, joinedload
 from sqlalchemy import select
+from typing import Optional, Any, Callable, TypeVar, Union
 
 from core.models import Article, User
 from core.serializers import (
@@ -10,9 +11,45 @@ from core.serializers import (
     ArticleInputSerializer,
 )
 
+F = TypeVar("F", bound=Callable[..., Any])
+ResponseType = Union[templating.Template, Status, Redirect]
+
+
+def with_session(func: F) -> F:
+    @wraps(func)
+    def wrapper(request: Request, *args, **kwargs):
+        with Session(request.app_data.engine) as session:
+            return func(request, session, *args, **kwargs)
+
+    return wrapper  # type: ignore
+
+
+def render_message(
+    request: Request,
+    success_message: Optional[str] = None,
+    failed_message: Optional[str] = None,
+) -> templating.Template:
+    return templating.render(
+        request,
+        "message.html.j2",
+        {"success_message": success_message, "failed_message": failed_message},
+    )
+
+
+def get_article_with_relations(session: Session, article_id: int) -> Optional[Article]:
+    stmt = (
+        select(Article)
+        .options(
+            joinedload(Article.author_relationship),
+            joinedload(Article.images),
+        )
+        .where(Article.id == article_id)
+    )
+    return session.execute(stmt).unique().scalars().first()
+
 
 @get("/nav")
-def nav(request: Request):
+def nav(request: Request) -> templating.Template:
     req_session = request.session()
     return templating.render(
         request,
@@ -24,47 +61,49 @@ def nav(request: Request):
 
 
 @get("/")
-def index(request: Request):
-    with Session(request.app_data.engine) as session:
-        stmt = select(Article).options(
-            joinedload(Article.author_relationship),
-            joinedload(Article.images),
-        )
-        articles = session.execute(stmt).unique().scalars().all()
-        serializer = ArticleModelSerializer(instance=articles, many=True)
-        session = request.session()
-        message = request.query()
-        return templating.render(
-            request,
-            "index.html.j2",
-            {
-                "articles": serializer.data,
-                "is_authenticate": session.get("is_authenticate"),
-                "message": message.get("message") if message else None,
-            },
-        )
+@with_session
+def index(request: Request, session: Session) -> templating.Template:
+    stmt = select(Article).options(
+        joinedload(Article.author_relationship),
+        joinedload(Article.images),
+    )
+    articles = session.execute(stmt).unique().scalars().all()
+    serializer = ArticleModelSerializer(instance=articles, many=True)
+    req_session = request.session()
+    message = request.query()
+
+    return templating.render(
+        request,
+        "index.html.j2",
+        {
+            "articles": serializer.data,
+            "is_authenticate": req_session.get("is_authenticate"),
+            "message": message.get("message") if message else None,
+        },
+    )
 
 
 @get("/login")
-def login(request: Request):
+def login(request: Request) -> templating.Template:
     return templating.render(request, "login.html.j2")
 
 
 @post("/login")
-def auth(request: Request):
+@with_session
+def auth(request: Request, session: Session) -> Union[Redirect, Status]:
     serializer = CredentialSerializer(request)
     serializer.validate()
-    with Session(request.app_data.engine) as session:
-        if user := session.query(User).filter_by(**serializer.validate_data).first():
-            req_session = request.session()
-            req_session["user_id"] = user.id
-            req_session["is_authenticate"] = True
-            return Redirect("/")
-        return Status.NOT_FOUND
+
+    if user := session.query(User).filter_by(**serializer.validate_data).first():
+        req_session = request.session()
+        req_session["user_id"] = user.id
+        req_session["is_authenticate"] = True
+        return Redirect("/")
+    return Status.NOT_FOUND
 
 
 @get("/article")
-def form_article(request: Request):
+def form_article(request: Request) -> templating.Template:
     message = request.query()
     return templating.render(
         request,
@@ -76,99 +115,81 @@ def form_article(request: Request):
 
 
 @post("/article")
-def create_article(request: Request):
+@with_session
+def create_article(request: Request, session: Session) -> templating.Template:
     serializer = ArticleInputSerializer(request)
     serializer.validate()
-    with Session(request.app_data.engine) as session:
-        new_article = Article(
-            **serializer.validate_data,
-            author=request.user_id
-        )
-        session.add(new_article)
-        session.commit()
-    return templating.render(request, "message.html.j2", {"success_message": "Article Created"})
+
+    new_article = Article(**serializer.validate_data, author=request.user_id)
+    session.add(new_article)
+    session.commit()
+
+    return render_message(request, success_message="Article Created")
 
 
 @get("/article/{id}")
-def retrieve_article(request: Request, id: int):
+@with_session
+def retrieve_article(
+    request: Request, session: Session, id: int
+) -> templating.Template:
     req_session = request.session()
-    with Session(request.app_data.engine) as session:
-        stmt = (
-            select(Article)
-            .options(
-                joinedload(Article.author_relationship),
-                joinedload(Article.images),
-            )
-            .where(Article.id == id)
-        )
-        if article := session.execute(stmt).unique().scalars().first():
-            serializer = ArticleModelSerializer(instance=article)
-            return templating.render(
-                request,
-                "article.html.j2",
-                {
-                    "article": serializer.data,
-                    "user_id": req_session.get("user_id"),
-                },
-            )
+    article = get_article_with_relations(session, id)
 
-        return templating.render(
-            request,
-            "article.html.j2",
-            {
-                "article": None,
-                "user_id": req_session.get("user_id"),
-            },
-        )
+    context = {
+        "article": ArticleModelSerializer(instance=article).data if article else None,
+        "user_id": req_session.get("user_id"),
+    }
+
+    return templating.render(request, "article.html.j2", context)
 
 
 @put("/article/{id}")
-def update_article(request: Request, id: int):
+@with_session
+def update_article(request: Request, session: Session, id: int) -> templating.Template:
     serializer = ArticleInputSerializer(request)
     serializer.validate()
-    with Session(request.app_data.engine) as session:
-        if article := session.query(Article).filter_by(id=id).first():
-            article.title = serializer.validate_data["title"]
-            article.content = serializer.validate_data["content"]
-            session.commit()
-            return templating.render(request, "message.html.j2", {"success_message": "Article Updated"})
-        return templating.render(request, "message.html.j2", {"failed_message": "Article not Found"})
+
+    if article := session.query(Article).filter_by(id=id).first():
+        for key, value in serializer.validate_data.items():
+            setattr(article, key, value)
+        session.commit()
+        return render_message(request, success_message="Article Updated")
+
+    return render_message(request, failed_message="Article not Found")
 
 
 @delete("/article/{id}")
-def delete_article(request: Request, id: int):
-    with Session(request.app_data.engine) as session:
-        if article := session.query(Article).filter_by(id=id).first():
-            session.delete(article)
-            session.commit()
-            return templating.render(request, "message.html.j2", {"success_message": "Article Deleted"})
-        return templating.render(request, "message.html.j2", {"failed_message": "Article not found"})
+@with_session
+def delete_article(request: Request, session: Session, id: int) -> templating.Template:
+    if article := session.query(Article).filter_by(id=id).first():
+        session.delete(article)
+        session.commit()
+        return render_message(request, success_message="Article Deleted")
+
+    return render_message(request, failed_message="Article not found")
 
 
 @post("/logout")
-def logout(request: Request):
+def logout(request: Request) -> Redirect:
     session = request.session()
     session.clear()
     return Redirect("/")
 
 
 @get("/article/{id}/edit")
-def edit_article(request: Request, id: int):
-    with Session(request.app_data.engine) as session:
-        stmt = (
-            select(Article)
-            .options(
-                joinedload(Article.author_relationship),
-                joinedload(Article.images),
-            )
-            .where(Article.id == id)
-        )
-        if article := session.execute(stmt).unique().scalars().first():
-            if article.author != request.user_id:
-                return Status.FORBIDDEN
+@with_session
+def edit_article(
+    request: Request, session: Session, id: int
+) -> Union[templating.Template, Status]:
+    article = get_article_with_relations(session, id)
 
-            serializer = ArticleModelSerializer(instance=article)
-            return templating.render(
-                request, "article_form.html.j2", {"article": serializer.data}
-            )
-    return Status.NOT_FOUND
+    if not article:
+        return Status.NOT_FOUND
+
+    if article.author != request.user_id:
+        return Status.FORBIDDEN
+
+    serializer = ArticleModelSerializer(instance=article)
+    return templating.render(
+        request, "article_form.html.j2", {"article": serializer.data}
+    )
