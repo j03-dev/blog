a9e6b8b2716b2c70a50db1ad27a81309f94fb9af
Fitahiana Nomeniavo Joe <24nomeniavo@gmail.com>
2025-10-16 19:04:13 +0300

    format

--- core/routers.py
+++ core/routers.py
@@ -10,7 +10,6 @@ def protect_page(request: Request, next, **kwargs):
     session = request.session()
     is_auth = session.get("is_auth")
     user_id = session.get("user_id")
-
     if is_auth:
         request.user_id = user_id
         return next(request, **kwargs)
